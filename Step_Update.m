75b06d88f66cd4370cfbe15426aadcde48f7f088
lpaul27 <lpaul27@LAB-SCI-202-02.whitworth.edu>
2025-06-12 09:57:43 -0700

    Reorganized

--- Step_Update.m
+++ Step_Update.m
@@ -36,40 +36,16 @@ for i = 1:NumCells
     yf(i,1) = y0(i,1) + (vy0(i,1) )*dt;
     
     %% Velocity update
-    % Natural angle: angle of self and friends, influenced by noise
+    % Natural angle: mean angle of self and friends, influenced by noise
     angNatural(i,1) = neibAngAvg(i,1) + eta * (rand() - 0.5)*pi;
     
-    % natural velocity of cell (excludes field
-    vx0(i,1) = vels_med * cos(angNatural(i,1));
-    vy0(i,1) = vels_med * sin(angNatural(i,1));
-    
-    % Updating position via kinematic equation
-    %x(i,1) = x_old(i,1) + (vx_old(i,1) + FxG(i,1))*dt; 
-    %y(i,1) = y_old(i,1) + (vy_old(i,1) + FyG(i,1))*dt;
-
-    %Angular noise term
-%     if(isnan(neibAng(i,1)))
-%          angNatural(i, 1) = (vel_ang(i, 1) +eta*(rand()-0.5)*pi);
-%     else
-%         %angNatural(i, 1) = mean(vel_ang(i, 1)+neighborWeight*neibAng(i,1)/ ...
-%          %   (neighborWeight + 1))+eta*(rand()-0.5)*pi;
-%         %angNatural(i, 1) = mean(vel_ang(i, 1)+neighborWeight*neibAng(i,1))+eta*(rand()-0.5)*pi;
-%         angNatural(i, 1) = vel_ang(i, 1)+eta*(rand()-0.5)*pi;
-%     end
-   % angNatural(i,1) = neibAng(i,1) + eta * (rand() - 0.5)*pi;
-    %Update of velocity component based on a random distribution of likely
-    %directions
-
-    % magnitude of vector for calculations with theta
-%    velocity_mag(i, 1) = sqrt(vx_old(i, 1).^2+vy_old(i, 1).^2);
-%     vx0(i,1) = velocity_mag(i,1) * cos(angNatural(i,1));
-%     vy0(i,1) = velocity_mag(i,1) * sin(angNatural(i,1));
-    %  vx0(i,1) = velsRange * cos(angNatural(i,1));
-    %  vy0(i,1) = velsRange * sin(angNatural(i,1));
+    % natural velocity of cell (excludes CC & CF)
+    vxNat(i,1) = vels_med * cos(angNatural(i,1));
+    vyNat(i,1) = vels_med * sin(angNatural(i,1));
 
     % New velocity vector based on how interaction forces affected angles
     % (componentwise)
-    vxf(i, 1) = (vx0(i,1) + FxG(i,1))* dt; % x component 
-    vyf(i, 1) = (vy0(i,1) + FyG(i,1))* dt;% y component
+    vxf(i, 1) = (vxNat(i,1) + FxG(i,1))* dt; % x component 
+    vyf(i, 1) = (vyNat(i,1) + FyG(i,1))* dt;% y component
 end
 end
